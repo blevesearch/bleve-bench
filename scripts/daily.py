6a6e414269ce18a01d131dd5ffcc757f9935af8f
Nimish Gupta <nimish@couchbase.com>
2015-10-21 16:58:27 +0530

    Adding std

--- scripts/daily.py
+++ scripts/daily.py
@@ -6,6 +6,7 @@ import time
 import subprocess
 import shutil
 import datetime as dt
+import numpy as np
 from optparse import OptionParser
 
 #TODO: some paths can be taken from environment variables
@@ -36,12 +37,20 @@ class Query:
         for f in fileList:
             total_bytes = 0
             total_time = 0
-            g = open(f, "r")
-            for line in g:
-                m = re.search("Result: (\w+) query - queries per second (\d+)", line)
-                if m != None:
-                    with open(self.data_path + "/" + os.path.basename(f) + "_" + m.group(1), "a+") as myfile:
-                        myfile.write(time.strftime("%m/%d/%Y") + "," + str(m.group(2)) + "\n")
+            result = []
+            for filename in f:
+                g = open(filename, "r")
+                for line in g:
+                    m = re.search("Result: (\w+) query - queries per second (\d+)", line)
+                    if m != None:
+                        result.append(m.group(2))
+            if len(result) > 0:
+                rate = max(result)
+                std = np.std(result)
+                print result
+            fname = os.path.basename(f).split("__")[0]
+            with open(self.data_path + "/" + fname + "_" + m.group(1), "a+") as myfile:
+                myfile.write(time.strftime("%Y%m%d") + "," + str(m.group(2)) + "\n")
 
     def runCmd(self):
         out = []
@@ -51,14 +60,18 @@ class Query:
         source = self.data["source"]
         qcount = self.data["querycount"]
         target_path = self.tmp_path + "/bench"
+        count = self.data["count"]
 
         for f in config_files:
             if os.path.exists(target_path):
                 shutil.rmtree(target_path)
-            output_file = os.path.basename(f) + ".res"
-            CMD = [binary_path, "-config", f, "-target", target_path, "-printTime", "5s", "-source", source, "-count", items, "-querycount", qcount]
-            execute_cmd(CMD, open(self.tmp_path + "/" + output_file, "w"))
-            out.append(self.tmp_path + "/" + output_file)
+            t = []
+            for f in range(count):
+                output_file = os.path.basename(f) + ".res" + "__" + str(count)
+                CMD = [binary_path, "-config", f, "-target", target_path, "-printTime", "5s", "-source", source, "-count", items, "-querycount", qcount]
+                execute_cmd(CMD, open(self.tmp_path + "/" + output_file, "w"))
+                t.append(self.tmp_path + "/" + output_file)
+            out.append(t)
         return self.parseOutput(out)
 
 
@@ -88,17 +101,25 @@ class Indexing:
         for f in fileList:
             total_bytes = 0
             total_time = 0
-            g = open(f, "r")
-            for line in g:
-                m = re.search("Result: (\d+) bytes in (\d+) seconds", line)
-                if m != None:
-                    total_bytes += int(m.group(1))
-                    total_time += int(m.group(2))
-            rate = 0
-            if total_time > 0:
-                rate = total_bytes/(total_time * 1000)
-            with open(self.data_path + "/" + os.path.basename(f), "a+") as myfile:
-                myfile.write(time.strftime("%m/%d/%Y") + "," + str(rate) + "\n")
+            result = []
+            for filename in f:
+                g = open(f, "r")
+                for line in g:
+                    m = re.search("Result: (\d+) bytes in (\d+) seconds", line)
+                    if m != None:
+                        total_bytes += int(m.group(1))
+                        total_time += int(m.group(2))
+                rate = 0
+                if total_time > 0:
+                    rate = total_bytes/(total_time * 1000)
+                    result.append(rate)
+            if len(result) > 0:
+                rate = max(result)
+                std = np.std(result)
+                print result
+            fname = os.path.basename(f).split("__")[0]
+            with open(self.data_path + "/" + fname, "a+") as myfile:
+                myfile.write(time.strftime("%Y%m%d") + "," + str(rate) + "\n")
 
     def runCmd(self):
         out = []
@@ -107,14 +128,18 @@ class Indexing:
         binary_path = self.data["binary"]
         source = self.data["source"]
         target_path = self.tmp_path + "/bench"
+        count = self.data["count"]
 
         for f in config_files:
             if os.path.exists(target_path):
                 shutil.rmtree(target_path)
-            output_file = os.path.basename(f) + ".res"
-            CMD = [binary_path, "-config", f, "-target", target_path, "-printTime", "5s", "-source", source, "-count", items]
-            execute_cmd(CMD, open(self.tmp_path + "/" + output_file, "w"))
-            out.append(self.tmp_path + "/" + output_file)
+            t = []
+            for f in range(count):
+                output_file = os.path.basename(f) + ".res" + "__" + str(count)
+                CMD = [binary_path, "-config", f, "-target", target_path, "-printTime", "5s", "-source", source, "-count", items]
+                execute_cmd(CMD, open(self.tmp_path + "/" + output_file, "w"))
+                t.append(self.tmp_path + "/" + output_file)
+            out.append(t)
         return self.parseOutput(out)
 
 class Conf:
